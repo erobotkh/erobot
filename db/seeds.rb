e73f1aae501d4bc8787fe1c26b6f2840fdf61244
Thea Choem <29684683+theachoem@users.noreply.github.com>
2022-08-28 12:35:02 +0700

    close #1 add oauth authentication (#2)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Doorkeeper::Application.count.zero?
+  Doorkeeper::Application.create(name: "Web client", redirect_uri: "", scopes: "")
+  Doorkeeper::Application.create(name: "Mobile client", redirect_uri: "", scopes: "")
+end
