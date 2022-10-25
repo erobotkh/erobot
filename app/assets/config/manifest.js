11eb7decd07065e6babf0294b17e192d10a4fe4a
Thea Choem <theacheng.g6@gmail.com>
2022-10-25 09:44:49 +0700

    fix: add administrate-field-simple_markdown/application.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,4 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link administrate-field-simple_markdown/application.css
+//= link administrate-field-simple_markdown/application.js
