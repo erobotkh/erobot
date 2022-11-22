724dc82a8b0b360eab0f2522c19e5a099059a016
Thea Choem <theacheng.g6@gmail.com>
2022-11-22 15:16:50 +0700

    final configuration

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
   draw(:api)
   draw(:frontend)
 
-  root 'landing#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
